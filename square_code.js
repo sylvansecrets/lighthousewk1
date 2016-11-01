866cc9e29cc88b20d5ed003248993895aec1a3d6
Richard Zhang <lapislacrima@gmail.com>
2016-11-01 23:46:43 +0000

    gives the appropriate side lengths

--- square_code.js
+++ square_code.js
@@ -1,8 +1,18 @@
 
 function square_code(str){
   noSpace = str.toLowerCase().split("").filter(function(c){
-    return(c.match(/[a-z]/))
+    return(c.match(/[a-z]/));
   })
-  return(noSpace);
+  l = noSpace.length;
+  l = 82;
+  side = Math.floor(Math.sqrt(l));
+  sides = [side,side];
+  if (sides[0]*sides[1]<l){
+    if (sides[0] < sides[1]){
+      sides[0] += 1;
+    } else {
+      sides[1] += 1;
+    }
+  }
+
 }
-console.log(square_code("This is a sentence."));
