81507b2e8126d6d9b5a3290a8ce2d4ebfe6d87bc
Richard Zhang <lapislacrima@gmail.com>
2016-11-02 00:06:11 +0000

    square cipher logic implemented; now accepts command line arguments if present

--- square_code.js
+++ square_code.js
@@ -1,18 +1,40 @@
 
 function square_code(str){
+  if (typeof(str)!=="String"){
+    str = str.join("")
+  }
   noSpace = str.toLowerCase().split("").filter(function(c){
     return(c.match(/[a-z]/));
   })
-  l = noSpace.length;
-  l = 82;
-  side = Math.floor(Math.sqrt(l));
-  sides = [side,side];
-  if (sides[0]*sides[1]<l){
+  var l = noSpace.length;
+  var side = Math.floor(Math.sqrt(l));
+  var sides = [side,side];
+  while (sides[0]*sides[1]<l){
     if (sides[0] < sides[1]){
       sides[0] += 1;
     } else {
       sides[1] += 1;
     }
   }
+  var output = []
+  for (var i=0; i<sides[1]; i+=1){
+    output.push([]);
+  }
 
+
+  noSpace.forEach(function(c,i){
+    output[Math.floor(i%sides[0])].push(c);
+  })
+
+  strOutput = []
+
+  output.forEach(function(c){
+    strOutput.push(c.join(""));
+  })
+  return strOutput;
+}
+
+
+if (input){
+  console.log(square_code(input));
 }
